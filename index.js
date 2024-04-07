7389a1f3acc8edad892394b8593a6bf5f00216c3
ren <154307471+ren-malthalash@users.noreply.github.com>
2024-04-07 20:17:23 +0800

    initial push

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const session = require('express-session');
+
+const hbs = require('hbs');
+
+const app = express();
+const port = 3000;
+
+app.use(session({
+
+}));
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(`${__dirname}/views/partials`);
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static('public'));
+
+app.use('/', routes);
+
+// binds the server to a specific port
+app.listen(port, function () {
+    console.log('app listening at port ' + port);
+});
