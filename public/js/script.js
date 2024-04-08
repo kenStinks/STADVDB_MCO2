91857cfe0b8ce006faf90e9df5271472c0d01057
Ken <134857008+kenStinks@users.noreply.github.com>
2024-04-08 18:28:07 +0800

    deletion function

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,25 @@
 //jquery when the document is ready
 $(document).ready(function(){
     console.log("Hello World!")
+
+    $('#edit_button').on("click", function(){
+        $('body').toggleClass("edit");
+        $('body').removeClass("delete");
+
+    })
+
+    $('#delete_button').on("click", function(){
+        $('body').toggleClass("delete");
+        $('body').removeClass("edit");
+    })
+
+    $('.table_entry').on("click", function(){
+        
+        if ($('body').hasClass('delete')){
+            $(this).remove(); //delete on user's end
+
+            //TODO: Delete in server (soft delete is safer)
+            console.log($(this).attr('id') + " has been deleted.")
+        }
+    })
 });
