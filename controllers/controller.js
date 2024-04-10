91a0ef15380c12b9e05ae71ebf1464b2fd8a68bd
Ken <134857008+kenStinks@users.noreply.github.com>
2024-04-10 14:54:38 +0800

    more ports

--- controllers/controller.js
+++ controllers/controller.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
     user:  process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    port:  process.env.MYSQL_PORT
+    port:  process.env.MYSQL_MAIN_PORT
 }).promise()
 
 async function getData(page,limit) {
