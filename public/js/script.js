696e4725cf7f914191a5cb986599a4321bf78d5f
ren <154307471+ren-malthalash@users.noreply.github.com>
2024-04-12 22:43:58 +0800

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -228,7 +228,7 @@ function timeToGeneral(date){
     var sMinutes = minutes.toString();
     if(hours<10) sHours = "0" + sHours;
     if(minutes<10) sMinutes = "0" + sMinutes;
-    return sHours + ":" + sMinutes + " " + AMPM;
+    return sHours + ":" + sMinutes + AMPM;
 }
 
 function timeToAMPM(date) {
