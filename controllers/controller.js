a42517464107832d37156068b24fa0132ce0d030
ren <154307471+ren-malthalash@users.noreply.github.com>
2024-04-12 14:01:46 +0800

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -560,11 +560,13 @@ const controller = {
         var transactionID = req.body.transactionID;
         var checkpointID = req.body.checkpointID;
 
+        const pool = mysql.createPool(poolHelper.pool_current);
+
         if (process.env.SERVER_NAME == 'Main' || process.env.SERVER_NAME == findNode(req.body.HospitalRegionName)) {
             
             logs.logTransaction(`${transactionID}|START|UPDATE`);
             try {
-                var connection = mysql.createPool(poolHelper.pool_current).getConnection();  
+                const connection = pool.getConnection();  
                 await connection.query('SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED');
                 await connection.beginTransaction();
     
