b1f98d5af84e841c07e05a3c92381d1e74b1ad60
ren <154307471+ren-malthalash@users.noreply.github.com>
2024-04-12 22:50:59 +0800

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -222,12 +222,14 @@ function timeToGeneral(date){
     var hours = Number(date.match(/^(\d+)/)[1]);
     var minutes = Number(date.match(/:(\d+)/)[1]);
     var AMPM = date.match(/\s(.*)$/)[1];
-    if(AMPM == "AM" && hours==12) hours = hours-12;
+    if (AMPM == "PM" && hours<12) {
+        hours = hours+12;
+    }
     var sHours = hours.toString();
     var sMinutes = minutes.toString();
     if(hours<10) sHours = "0" + sHours;
     if(minutes<10) sMinutes = "0" + sMinutes;
-    return sHours + ":" + sMinutes;
+    return sHours + ":" + sMinutes + ":00";
 }
 
 function timeToAMPM(date) {
